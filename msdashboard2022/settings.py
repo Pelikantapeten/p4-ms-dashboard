71d642bd0c5fb2b8c97e8c664f58d778f03e6edf
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-02 20:33:44 +0000

    Uppdate SECRET_KEY in settings.py refference env.py

--- msdashboard2022/settings.py
+++ msdashboard2022/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q@2nytepn=r5@c9q(^0taw6$6)6$9257)&##db(gkik84ul4wn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
