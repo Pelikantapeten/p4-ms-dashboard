7a9da5c2bc73bcd22d917986b2c43e7843e76d85
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-02 20:49:36 +0000

    Update database section in settings.py to connect to HerokuPostgres

--- msdashboard2022/settings.py
+++ msdashboard2022/settings.py
@@ -79,13 +79,16 @@ WSGI_APPLICATION = 'msdashboard2022.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
