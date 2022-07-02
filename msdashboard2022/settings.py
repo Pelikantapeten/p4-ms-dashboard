b2d31051ea793742ccd96cdb8fd8d81903f35939
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-02 21:10:17 +0000

    Update settings.py with Heroku app as allowed host

--- msdashboard2022/settings.py
+++ msdashboard2022/settings.py
@@ -19,6 +19,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ms-dashboard2022.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'msdashboard2022.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
