a2c9d8ed400164e9e7b2e4faf6b148bd284af80a
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-11 10:51:42 +0000

    Add registration form for admin user in forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -44,3 +44,39 @@ class RegisterForm(forms.ModelForm):
         if password is not None and password != password_2:
             self.add_error("password_2", "Your passwords must match")
         return cleaned_data
+
+
+class UserAdminCreationForm(forms.ModelForm):
+    """
+    A form for creating admin users. Includes all the required
+    fields, plus a repeated password.
+    """
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_2 = forms.CharField(
+        label='Confirm Password', widget=forms.PasswordInput
+        )
+
+    class Meta:
+        """
+        Meta class for fields
+        """
+        model = User
+        fields = ['email']
+
+    def clean(self):
+        '''
+        Verify both passwords match.
+        '''
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_2 = cleaned_data.get("password_2")
+        if password is not None and password != password_2:
+            self.add_error("password_2", "Your passwords must match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
