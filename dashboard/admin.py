3694a4575127f2db3967c5f4f80305c29e0c289c
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-11 13:17:19 +0000

    Add imports and hide 'Groups' in admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,15 @@
+"""
+Admin rules for dashboard app
+"""
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+
+User = get_user_model()
+
+
+# Remove Group Model from admin. We're not using it.
+admin.site.unregister(Group)
+
