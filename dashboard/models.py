46ec0da9e082552d7a2a89723a0449b03caf5366
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-11 10:23:29 +0000

    Add model for Mentor and Admin users

--- dashboard/models.py
+++ dashboard/models.py
@@ -10,3 +10,59 @@ from django.contrib.auth.models import (
 )
 # Used to generate URLs by reversing the URL patterns
 from django.urls import reverse
+
+
+class User(AbstractBaseUser):
+    """
+    Class for users (Mentors and Course admins)
+    """
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    staff = models.BooleanField('Mentor', default=False)
+    admin = models.BooleanField('Course manager', default=False)
+    name = models.CharField('First Name', max_length=80, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'  # Username is set to email
+    REQUIRED_FIELDS = []  # Email and Password are required fields
+
+    def get_full_name(self):
+        """
+        User is identified by email
+        """
+        return self.email
+
+    def get_short_name(self):
+        """
+        The user is identified by their email address
+        """
+        return self.email
+
+    def __str__(self):
+        """
+        Return email as string
+        """
+        return str(self.email)
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.staff
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+
