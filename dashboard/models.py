cf83c99522a0110e20e9b4f1984af0a246d427b3
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-11 08:25:25 +0000

    Add imports in models.py for user and student models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
+"""
+Models for app dashboard
+"""
+import uuid  # Required for creating unique id for mentor/student session
 from django.db import models
-
-# Create your models here.
+#  from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+)
+# Used to generate URLs by reversing the URL patterns
+from django.urls import reverse
