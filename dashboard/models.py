1e3f835c448555119b4d2a2ba93d930e30061d37
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-11 10:25:45 +0000

    Add model for User manager and making first migrations

--- dashboard/models.py
+++ dashboard/models.py
@@ -12,6 +12,53 @@ from django.contrib.auth.models import (
 from django.urls import reverse
 
 
+class UserManager(BaseUserManager):
+    """
+    Model for managing users
+    """
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password and name.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password):
+        """
+        Creates and saves a mentor(staff) user with the
+        given email and password and name.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a course admin(superuser)
+        with the given email and password and name.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.admin = True
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser):
     """
     Class for users (Mentors and Course admins)
