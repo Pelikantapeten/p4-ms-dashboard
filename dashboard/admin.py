45f06bd62f6d0a27a1d73aaf525b67b7fa0b84f9
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-11 13:28:34 +0000

    Add UserAdmin class to admin.py for creation and change of user

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,6 +2,7 @@
 Admin rules for dashboard app
 """
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
@@ -13,3 +14,34 @@ User = get_user_model()
 # Remove Group Model from admin. We're not using it.
 admin.site.unregister(Group)
 
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Class to display the UserAdmin options for create and change
+    users as Admin
+    """
+    
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    list_display = ['email', 'admin', 'staff', 'created_on']
+    list_filter = ['admin', 'staff']
+    fieldsets = (
+        ('User credentials', {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Type of user', {'fields': ('admin', 'staff',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        ('Enter User credentials', {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password_2')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Type of User', {'fields': ('admin', 'staff',)}),
+    )
+    search_fields = ['email']
+    ordering = ['email']
+    filter_horizontal = ()
