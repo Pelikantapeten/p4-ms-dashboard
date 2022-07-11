7f1adecda084b422bb8364efccbc4a8b0f188cab
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-11 14:25:35 +0000

    Add import of models to admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Student, Mentor, StudentMentorCard, StudentSession
 
 from .forms import UserAdminCreationForm, UserAdminChangeForm
 
