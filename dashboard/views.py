5253745e207477215290603eb1446181ae982c58
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-13 13:04:09 +0000

    Add views for mentorcards and imports in views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,8 @@
 Views for app Dashboard
 """
 from django.shortcuts import render
+from django.views import generic
+from dashboard.models import StudentMentorCard
 
 
 def start_page(request):
@@ -9,3 +11,17 @@ def start_page(request):
     View for home page.
     """
     return render(request, 'index.html')
+
+
+class MentorCard(generic.ListView):
+    """
+    View class for mentorcards
+    """
+    model = StudentMentorCard
+
+
+class MentorCardDetails(generic.DetailView):
+    """
+    Detailed view class for mentorcards
+    """
+    model = StudentMentorCard
