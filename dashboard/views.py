50351e58f147e21bc147fb534e3014950cb593f0
Pelikantapeten <mats.simonsson@sweora.se>
2022-07-16 07:39:20 +0000

    Add view for create_session_form in views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -42,3 +42,30 @@ def session_detail(request, id):
     """
     id = StudentSession.objects.get(id=id)
     return render(request, 'session_detail.html', {'studentsession': id})
+
+
+def session_form_view(request):
+    """
+    View for create Student Session page
+    Created this by looking at the view created by CluelessBiker,
+    in her project:
+    https://github.com/CluelessBiker/project4-print-statements
+    """
+    if request.method == 'POST':
+        create_session_form = CreateSession(request.POST, request.FILES)
+        if create_session_form.is_valid():
+            create_session_form.instance.mentor = request.user
+            create_session_form.save()
+            messages.success(
+                request, "You have created a new session")
+            return redirect('session-list')
+        else:
+            create_session_form = CreateSession()
+
+    return render(
+        request,
+        'sessionsubmit.html',
+        {
+            'create_session_form': CreateSession(),
+        },
+    )
