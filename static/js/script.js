d72f52aa1f37b22535922def40b6f655e7f4ff8c
Pelikantapeten <mats.simonsson@sweora.se>
2022-08-15 20:01:47 +0000

    Change script to make row a link in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 // Code gotten from https://codepen.io/NaokiIshimura/pen/aEvQPY
+// Shows error but code has not been altered from the source
 $(document).ready(function($) {
     $(".table-row").click(function() {
         window.document.location = $(this).data("href");
