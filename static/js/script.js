ffa091a13b032cf3ccacf268972da4ec68883501
Pelikantapeten <mats.simonsson@sweora.se>
2022-08-19 09:50:06 +0000

    Add function in script.js for pagination thru tabs in session_details

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,12 @@
-// Code gotten from https://codepen.io/NaokiIshimura/pen/aEvQPY
-// Shows error but code has not been altered from the source
+
 $(document).ready(function($) {
+    // Code gotten from https://codepen.io/NaokiIshimura/pen/aEvQPY
+    // Shows error but code has not been altered from the source
     $(".table-row").click(function() {
         window.document.location = $(this).data("href");
+    });
+    $('#session-detail-list a').on('click', function (e) {
+        e.preventDefault()
+        $(this).tab('show')
     });
 });
